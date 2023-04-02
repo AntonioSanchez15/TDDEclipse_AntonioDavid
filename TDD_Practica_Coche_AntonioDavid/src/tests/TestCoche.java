72f08de64b001d1fc7e54b3f8b4e9284dba392a4
AntonioSanchez15 <117301120+AntonioSanchez15@users.noreply.github.com>
2023-04-02 21:44:11 +0200

    Refactorización de nombre de métodos con mi nombre

--- TDD_Practica_Coche_AntonioDavid/src/tests/TestCoche.java
+++ TDD_Practica_Coche_AntonioDavid/src/tests/TestCoche.java
@@ -8,31 +8,31 @@ import org.junit.jupiter.api.Test;
 class TestCoche {
 
 	@Test
-	void testAlCrearCocheSuVelocidadEsCero() {
+	void testAlCrearCocheSuVelocidadEsCero_AntonioaDavid() {
 		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 	
 	@Test
-	void testAlacelerarUnCocheSuVelocidadAumenta() {
+	void testAlacelerarUnCocheSuVelocidadAumenta_AntonioDavid() {
 		Coche nuevoCoche = new Coche();
-		nuevoCoche.acelerar(30);
+		nuevoCoche.acelerar_AntonioDavid(30);
 		Assertions.assertEquals(30, nuevoCoche.velocidad);
 	}
 	
 	@Test
-	void testAlacelerarUnCocheSuVelocidadDisminuye() {
+	void testAlacelerarUnCocheSuVelocidadDisminuye_AntonioDavid() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad=50;
-		nuevoCoche.decelerar(20);
+		nuevoCoche.decelerar_AntonioDavid(20);
 		Assertions.assertEquals(30, nuevoCoche.velocidad);
 	}
 	
 	@Test
-	void testAlacelerarUnCocheSuVelocidadPuedeSerMenorQueCero() {
+	void testAlacelerarUnCocheSuVelocidadPuedeSerMenorQueCero_AntonioDavid() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad=50;
-		nuevoCoche.decelerar(80);
+		nuevoCoche.decelerar_AntonioDavid(80);
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 	
